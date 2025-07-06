86496da72fb186a124b801e077d6ed7664ec7f8e
Ryan Goree <goree.ryan@gmail.com>
2025-07-05 19:05:12 -0500

    Update indent style

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,68 +3,68 @@ import test, { suite } from 'node:test';
 import { parseBigInt } from './index.ts';
 
 test('simple', () => {
-	assert.strictEqual(parseBigInt('1'), 1n);
-	assert.strictEqual(parseBigInt('-1'), -1n);
-	assert.strictEqual(parseBigInt('1_000'), 1000n);
-	assert.strictEqual(parseBigInt('-1_000'), -1_000n);
-	assert.strictEqual(parseBigInt('5,500,000,000'), 5_500_000_000n);
-	assert.strictEqual(parseBigInt('-5,500,000,000'), -5_500_000_000n);
+  assert.strictEqual(parseBigInt('1'), 1n);
+  assert.strictEqual(parseBigInt('-1'), -1n);
+  assert.strictEqual(parseBigInt('1_000'), 1000n);
+  assert.strictEqual(parseBigInt('-1_000'), -1_000n);
+  assert.strictEqual(parseBigInt('5,500,000,000'), 5_500_000_000n);
+  assert.strictEqual(parseBigInt('-5,500,000,000'), -5_500_000_000n);
 });
 
 suite('scientific notation', () => {
-	test('integers', () => {
-		assert.strictEqual(parseBigInt('1e0'), 1n);
-		assert.strictEqual(parseBigInt('1e3'), 1_000n);
-		assert.strictEqual(parseBigInt('1e18'), 1n * 10n ** 18n);
-		assert.strictEqual(parseBigInt('-1e18'), -1n * 10n ** 18n);
-		assert.strictEqual(parseBigInt('50,000e18'), 50_000n * 10n ** 18n);
-		assert.strictEqual(parseBigInt('-50,000e18'), -50_000n * 10n ** 18n);
-		assert.strictEqual(parseBigInt('50_000e18'), 50_000n * 10n ** 18n);
-		assert.strictEqual(parseBigInt('-50_000e18'), -50_000n * 10n ** 18n);
-	});
+  test('integers', () => {
+    assert.strictEqual(parseBigInt('1e0'), 1n);
+    assert.strictEqual(parseBigInt('1e3'), 1_000n);
+    assert.strictEqual(parseBigInt('1e18'), 1n * 10n ** 18n);
+    assert.strictEqual(parseBigInt('-1e18'), -1n * 10n ** 18n);
+    assert.strictEqual(parseBigInt('50,000e18'), 50_000n * 10n ** 18n);
+    assert.strictEqual(parseBigInt('-50,000e18'), -50_000n * 10n ** 18n);
+    assert.strictEqual(parseBigInt('50_000e18'), 50_000n * 10n ** 18n);
+    assert.strictEqual(parseBigInt('-50_000e18'), -50_000n * 10n ** 18n);
+  });
 
-	test('decimal point', () => {
-		assert.strictEqual(parseBigInt('1.0e1'), 10n);
-		assert.strictEqual(parseBigInt('1.1e18'), 11n * 10n ** 17n);
-		assert.strictEqual(
-			parseBigInt('333_333.555_555e18'),
-			333_333_555_555n * 10n ** 12n,
-		);
-		assert.strictEqual(
-			parseBigInt('-333_333.555_555e18'),
-			-333_333_555_555n * 10n ** 12n,
-		);
-	});
+  test('decimal point', () => {
+    assert.strictEqual(parseBigInt('1.0e1'), 10n);
+    assert.strictEqual(parseBigInt('1.1e18'), 11n * 10n ** 17n);
+    assert.strictEqual(
+      parseBigInt('333_333.555_555e18'),
+      333_333_555_555n * 10n ** 12n,
+    );
+    assert.strictEqual(
+      parseBigInt('-333_333.555_555e18'),
+      -333_333_555_555n * 10n ** 12n,
+    );
+  });
 
-	test('negative exponent', () => {
-		assert.throws(() => parseBigInt('1e-1'));
-		assert.strictEqual(parseBigInt('10e-1'), 1n);
-		assert.strictEqual(parseBigInt('-1000e-2'), -10n);
-	});
+  test('negative exponent', () => {
+    assert.throws(() => parseBigInt('1e-1'));
+    assert.strictEqual(parseBigInt('10e-1'), 1n);
+    assert.strictEqual(parseBigInt('-1000e-2'), -10n);
+  });
 });
 
 suite('base prefixes', () => {
-	test('binary', () => {
-		assert.strictEqual(parseBigInt('0b1'), 1n);
-		assert.strictEqual(parseBigInt('0b10'), 2n);
-		assert.strictEqual(parseBigInt('0b11'), 3n);
-		assert.strictEqual(parseBigInt('0b1111_1111'), 255n);
-		assert.strictEqual(parseBigInt('-0b1111_1111'), -255n);
-	});
+  test('binary', () => {
+    assert.strictEqual(parseBigInt('0b1'), 1n);
+    assert.strictEqual(parseBigInt('0b10'), 2n);
+    assert.strictEqual(parseBigInt('0b11'), 3n);
+    assert.strictEqual(parseBigInt('0b1111_1111'), 255n);
+    assert.strictEqual(parseBigInt('-0b1111_1111'), -255n);
+  });
 
-	test('octal', () => {
-		assert.strictEqual(parseBigInt('0o1'), 1n);
-		assert.strictEqual(parseBigInt('0o10'), 8n);
-		assert.strictEqual(parseBigInt('0o11'), 9n);
-		assert.strictEqual(parseBigInt('0o777'), 511n);
-		assert.strictEqual(parseBigInt('-0o777'), -511n);
-	});
+  test('octal', () => {
+    assert.strictEqual(parseBigInt('0o1'), 1n);
+    assert.strictEqual(parseBigInt('0o10'), 8n);
+    assert.strictEqual(parseBigInt('0o11'), 9n);
+    assert.strictEqual(parseBigInt('0o777'), 511n);
+    assert.strictEqual(parseBigInt('-0o777'), -511n);
+  });
 
-	test('hex', () => {
-		assert.strictEqual(parseBigInt('0x1'), 1n);
-		assert.strictEqual(parseBigInt('0x10'), 16n);
-		assert.strictEqual(parseBigInt('0x11'), 17n);
-		assert.strictEqual(parseBigInt('0xff'), 255n);
-		assert.strictEqual(parseBigInt('-0xff'), -255n);
-	});
+  test('hex', () => {
+    assert.strictEqual(parseBigInt('0x1'), 1n);
+    assert.strictEqual(parseBigInt('0x10'), 16n);
+    assert.strictEqual(parseBigInt('0x11'), 17n);
+    assert.strictEqual(parseBigInt('0xff'), 255n);
+    assert.strictEqual(parseBigInt('-0xff'), -255n);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -15,75 +15,75 @@
  * ```
  */
 export function parseBigInt(input: bigint | number | string): bigint {
-	switch (typeof input) {
-		case 'bigint':
-			return input;
+  switch (typeof input) {
+    case 'bigint':
+      return input;
 
-		case 'number':
-			return BigInt(input);
+    case 'number':
+      return BigInt(input);
 
-		case 'string': {
-			let n = input.trim().toLowerCase().replace(/(_|,)/g, '');
+    case 'string': {
+      let n = input.trim().toLowerCase().replace(/(_|,)/g, '');
 
-			let sign = 1n;
-			if (n.startsWith('-')) {
-				sign = -1n;
-				n = n.slice(1);
-			}
-			if (n.startsWith('+')) {
-				n = n.slice(1);
-			}
+      let sign = 1n;
+      if (n.startsWith('-')) {
+        sign = -1n;
+        n = n.slice(1);
+      }
+      if (n.startsWith('+')) {
+        n = n.slice(1);
+      }
 
-			// Handle scientific notation.
-			if (n.includes('e') && !n.startsWith('0x')) {
-				const [mantissaStr = '', exponentStr = '', invalidExponent] =
-					n.split('e');
+      // Handle scientific notation.
+      if (n.includes('e') && !n.startsWith('0x')) {
+        const [mantissaStr = '', exponentStr = '', invalidExponent] =
+          n.split('e');
 
-				// Validate input format.
-				if (invalidExponent || !/^-?\d*$/.test(exponentStr)) {
-					throw new Error(
-						`Invalid exponent in ${input}: ${n.replace(/^[^e]+e/, '')}`,
-					);
-				}
-				if (!/^\d+(\.\d+)?$/.test(mantissaStr)) {
-					throw new Error(`Invalid number format: ${input}`);
-				}
+        // Validate input format.
+        if (invalidExponent || !/^-?\d*$/.test(exponentStr)) {
+          throw new Error(
+            `Invalid exponent in ${input}: ${n.replace(/^[^e]+e/, '')}`,
+          );
+        }
+        if (!/^\d+(\.\d+)?$/.test(mantissaStr)) {
+          throw new Error(`Invalid number format: ${input}`);
+        }
 
-				// To handle cases like "12300e-2", the trailing zeroes are separated
-				// from the integer version of the significand and used to adjust the
-				// exponent since BigInt doesn't support negative exponents.
-				const [integerSignificandStr = '', trailingZeroesStr = ''] = mantissaStr
-					.replace('.', '')
-					.split(/(?<=[1-9]+)(?=0+$)/);
+        // To handle cases like "12300e-2", the trailing zeroes are separated
+        // from the integer version of the significand and used to adjust the
+        // exponent since BigInt doesn't support negative exponents.
+        const [integerSignificandStr = '', trailingZeroesStr = ''] = mantissaStr
+          .replace('.', '')
+          .split(/(?<=[1-9]+)(?=0+$)/);
 
-				// Adjust the exponent to account for the decimal point's position and
-				// any trailing zeroes that were part of the original mantissa.
-				const [_, fractionStr = ''] = mantissaStr.split('.');
-				const adjustedExponent =
-					BigInt(exponentStr) -
-					BigInt(fractionStr.length) +
-					BigInt(trailingZeroesStr.length);
+        // Adjust the exponent to account for the decimal point's position and
+        // any trailing zeroes that were part of the original mantissa.
+        const [_, fractionStr = ''] = mantissaStr.split('.');
+        const adjustedExponent =
+          BigInt(exponentStr) -
+          BigInt(fractionStr.length) +
+          BigInt(trailingZeroesStr.length);
 
-				// A negative adjusted exponent implies a non-integer.
-				if (adjustedExponent < 0) {
-					const exponentNumber = Number(adjustedExponent);
-					const unscaledStr = integerSignificandStr.padStart(
-						Math.abs(exponentNumber) + 1,
-						'0',
-					);
-					const integerStr = unscaledStr.slice(0, exponentNumber);
-					const fractionStr = unscaledStr.slice(exponentNumber);
-					throw new Error(`Invalid BigInt: ${integerStr}.${fractionStr}`);
-				}
+        // A negative adjusted exponent implies a non-integer.
+        if (adjustedExponent < 0) {
+          const exponentNumber = Number(adjustedExponent);
+          const unscaledStr = integerSignificandStr.padStart(
+            Math.abs(exponentNumber) + 1,
+            '0',
+          );
+          const integerStr = unscaledStr.slice(0, exponentNumber);
+          const fractionStr = unscaledStr.slice(exponentNumber);
+          throw new Error(`Invalid BigInt: ${integerStr}.${fractionStr}`);
+        }
 
-				return BigInt(integerSignificandStr) * 10n ** adjustedExponent * sign;
-			}
+        return BigInt(integerSignificandStr) * 10n ** adjustedExponent * sign;
+      }
 
-			try {
-				return BigInt(n) * sign;
-			} catch {
-				throw new Error(`Invalid number format: ${input}`);
-			}
-		}
-	}
+      try {
+        return BigInt(n) * sign;
+      } catch {
+        throw new Error(`Invalid number format: ${input}`);
+      }
+    }
+  }
 }
